bc621ef2f614144462c76c79587fb2c68bc01906
Kist221 <Kist221@gmail.com>
2018-01-19 21:22:20 -0700

    still no idea what I'm supposed to do

---
+++ Random.js
@@ -0,0 +1,9 @@
+// array of game choices
+var gameChoices = [ 'aqua','beige','bisque','black','brown','chartreuse','coral','crimson','fuchsia','gold','gray','green','grey','honeydew','indigo','ivory','khaki','lavender','lime','linen','magenta','maroon','olive','orange','orchid','peach','pink','plum','purple','red','salmon','sienna','silver','snow','tan','turquoise','violet','wheat','white','yellow'];
+
+// Randomly chooses a choice from the options array. This is the Computer's guess.
+var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+
+
+// export guess
+module.exports = computerGuess;
--- Word.js
+++ Word.js
@@ -1,1 +1,42 @@
-  * **Word**: Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
+// require word generator
+var computerGuess = require("./Random.js");
+
+
+  // * **Word**: Used to create an object representing the current word the user is attempting to guess. This should contain word specific logic and data.
+
+
+// Constructor the makes the words
+var Word = function (word) {
+	// store word in variable
+	this.word = word.toUpperCase().trim();
+	// represent word as array of letters
+	this.wordLetters = [];
+	this.wordHidden = [];
+	console.log(this.word.length);
+	// convert word to underscores
+	// check if current word
+	if (wordLetters.length === 0) {
+		console.log(true);
+		// loop length of word
+		for (var i = 0; i < this.word.length; i++) {
+			// push each letter of word into array
+			wordLetters.push(this.word.charAt([i]));
+			console.log(wordLetters);
+			// also create the hidden word array
+			if (wordLetters[i] !== -1) {
+				wordHidden.push("_");
+				console.log(wordHidden);
+			}
+		}
+	}
+}
+
+Word(computerGuess);
+
+
+// for each letter make an underscore array
+
+
+
+
+module.exports = Word;
